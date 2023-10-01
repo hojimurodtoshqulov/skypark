ea35875c5f646049817ff070c504076e69db86a4
bakhodir <bahodirabdullayev0556@gmail.com>
2023-10-02 00:00:45 +0500

    navbar dropdown and cardZone, attractions

--- src/components/cardZone/cardZone.jsx
+++ src/components/cardZone/cardZone.jsx
@@ -1,13 +1,23 @@
 import scss from "./cardZone.module.scss";
 import Image from "next/image";
 const CardZone = ({ img, title, text }) => {
-	return (
-		<div className={scss.card}>
-			<Image src={img} width={580} height={354} alt={img} loading="lazy" />
-			<h3>{title}</h3>
-			<p>{text}</p>
-		</div>
-	);
+  return (
+    <div className={scss.card}>
+      <div>
+        <Image
+          layout="responsive"
+          src={img}
+          width={580}
+          height={354}
+          alt={img}
+          loading="lazy"
+        />
+      </div>
+
+      <h3>{title}</h3>
+      <p>{text}</p>
+    </div>
+  );
 };
 
 export default CardZone;
