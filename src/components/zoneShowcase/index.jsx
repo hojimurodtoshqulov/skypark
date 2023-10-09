c27c230e81a98de387473b699b511273d8f797d8
bakhodir <bahodirabdullayev0556@gmail.com>
2023-10-09 19:12:20 +0500

    zone showcase text animation

--- src/components/zoneShowcase/index.jsx
+++ src/components/zoneShowcase/index.jsx
@@ -1,16 +1,37 @@
+import { AnimatePresence, motion } from "framer-motion";
 import s from "./zoneShowcase.module.scss";
 
 const ZoneShowcase = ({ data }) => {
   return (
-    <div className={s.wrapper}>
-      <div className="container">
-        <div className={s.mini__container}>
-          <h3>{data?.title}</h3>
-          <h2>{data?.heading}</h2>
-          <p>{data?.text}</p>
+    <AnimatePresence>
+      <div className={s.wrapper}>
+        <div className="container">
+          <div className={s.mini__container}>
+            <motion.h3
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.4, delay: 6.5 }}
+            >
+              {data?.title}
+            </motion.h3>
+            <motion.h2
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.4, delay: 6.65 }}
+            >
+              {data?.heading}
+            </motion.h2>
+            <motion.p
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.4, delay: 6.8 }}
+            >
+              {data?.text}
+            </motion.p>
+          </div>
         </div>
       </div>
-    </div>
+    </AnimatePresence>
   );
 };
 
