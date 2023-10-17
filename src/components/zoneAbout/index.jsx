6c325a47d387905b6a6bfcaa99f708a06356a534
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-10-17 11:55:55 +0500

    updated

--- src/components/zoneAbout/index.jsx
+++ src/components/zoneAbout/index.jsx
@@ -1,36 +1,52 @@
 import { Fragment } from "react";
 import s from "./ZoneAbout.module.scss";
 import Image from "next/image";
+import { useState } from "react";
 
 const ZoneAbout = ({ about }) => {
-  return (
-    <>
-      <div className={s.wrapper}>
-        {about &&
-          about.map((a, i) => (
-            <div className={s.card} key={i}>
-              <div className="container">
-                <h2>{a.h2}</h2>
-              </div>
-              <div className={s.flex}>
-                <div className={s.img}>
-                  <Image
-                    width={666}
-                    height={636}
-                    layout="responsive"
-                    src={a.image}
-                  />
-                </div>
-                <div className={s.text}>
-                  <h3>{a.h4}</h3>
-                  <p>{a.p}</p>
-                </div>
-              </div>
-            </div>
-          ))}
-      </div>
-    </>
-  );
+	const [select, setSelect] = useState(0);
+	return (
+		<>
+			<div className={s.wrapper}>
+				{about &&
+					about.map((a, i) => (
+						<div className={s.card} key={i}>
+							<div className="container">
+								<h2>{a.h2}</h2>
+							</div>
+							<div className={s.flex}>
+								{}
+								<div
+									className={s.img}
+									onClick={() => {
+										select === a.image.length - 1
+											? setSelect(0)
+											: setSelect((prev) => prev + 1);
+									}}
+								>
+									<Image
+										width={666}
+										height={636}
+										layout="responsive"
+										src={a.image[select]}
+										alt={a.image[select]}
+										key={select}
+										initial={{ opacity: 0 }}
+										animate={{ opacity: 1 }}
+										exit={{ opacity: 0 }}
+										transition={{ duration: 0.4 }}
+									/>
+								</div>
+								<div className={s.text}>
+									<h3>{a.h4}</h3>
+									<p>{a.p}</p>
+								</div>
+							</div>
+						</div>
+					))}
+			</div>
+		</>
+	);
 };
 
 export default ZoneAbout;
