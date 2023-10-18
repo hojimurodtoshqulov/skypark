b9822f64f94085d109b6f133faf95ddafc77b474
bakhodir <bahodirabdullayev0556@gmail.com>
2023-10-18 23:01:10 +0500

    about us showcase(full), cartificates(non-responsive)

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,17 +1,27 @@
 import Month from "@/components/month/month";
 import AboutUs from "@/components/aboutUs/aboutUs";
 import Principle from "@/components/Principle";
+import ZoneShowcase from "@/components/zoneShowcase";
+import Certificates from "@/components/certificates";
 
 const About = () => {
-	return (
-		<>
-				<div style={{ padding: "100px 0 200px" }}>
-					<AboutUs />
-					<Principle />
-					<Month />
-				</div>
-		</>
-	);
+  return (
+    <>
+      <div style={{ padding: "100px 0 200px" }}>
+        <ZoneShowcase data={showcaseData} />
+        <AboutUs />
+        <Principle />
+        <Month />
+        <Certificates />
+      </div>
+    </>
+  );
 };
 
 export default About;
+
+const showcaseData = {
+  title: "О нас",
+  heading: "Крупнейшие аттракционы в городе",
+  text: "Преврати свой день в незабываемое путешествие посетив наши аттракционы!",
+};
