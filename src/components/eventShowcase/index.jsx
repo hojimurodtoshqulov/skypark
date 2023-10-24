8cebdcb2f0879ddccaa27a064abaa7d3147f2c9e
bakhodir <bahodirabdullayev0556@gmail.com>
2023-10-24 22:57:53 +0500

    events page

--- src/components/eventShowcase/index.jsx
+++ src/components/eventShowcase/index.jsx
@@ -2,20 +2,15 @@ import Image from "next/image";
 import s from "./eventShowcase.module.scss";
 import { useTranslation } from "next-i18next";
 // import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-const EventShowcase = () => {
+const EventShowcase = ({ data }) => {
   return (
     <div className={s.warapper}>
       <div className={`container ${s.showcase}`}>
-        <h2>Искусство Иллюзий</h2>
-        <p>Многообразие зон потребности гостей всех возрастов</p>
+        <h2>{data?.title}</h2>
+        <p>{data?.text}</p>
       </div>
       <div className={s.image}>
-        <Image
-          src="/images/gameZone1.png"
-          width={1440}
-          height={520}
-          layout="responsive"
-        />
+        <Image src={data?.img} width={1440} height={520} layout="responsive" />
       </div>
       <div className={s.showcase}>
         <span></span>
--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Month from "@/components/month/month";
 import AnimationSection from "@/components/animationSection/animationSection";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
+import EventShowcase from "@/components/eventShowcase";
 
 export const getStaticProps = async ({ locale }) => ({
-	props: {
-		...(await serverSideTranslations(locale, ["common"])),
-	},
+  props: {
+    ...(await serverSideTranslations(locale, ["common"])),
+  },
 });
 
 const Events = () => {
-	const { t } = useTranslation();
-	return (
-		<>
-			<AnimationSection>
-				<div style={{ padding: "100px 0 200px" }}>
-					<Month />
-				</div>
-			</AnimationSection>
-		</>
-	);
+  const { t } = useTranslation();
+  const [elementData, setElementData] = useState(null);
+
+  const handleClick = (data) => {
+    if (typeof window !== "undefined") {
+      setElementData(data);
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  };
+
+  return (
+    <>
+      <AnimationSection>
+        <div style={{ padding: "100px 0 200px" }}>
+          {elementData ? <EventShowcase data={elementData} /> : null}
+          <Month handleClick={handleClick} />
+        </div>
+      </AnimationSection>
+    </>
+  );
 };
 
 export default Events;
