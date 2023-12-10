7142574991c5f31891178b273cef92c353d757a7
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-12-10 23:17:10 +0500

    updated

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import scss from "./layout.module.scss";
 import Footer from "./Footer";
 import CTA from "@/components/cta";
+// import video from "../assets/VM_animation.mp4";
 
 export default function Layout({ children }) {
 	const [active, setActive] = useState(true);
@@ -17,12 +18,14 @@ export default function Layout({ children }) {
 			{active ? (
 				<div className={scss.BG_video}>
 					<div></div>
-					<video muted playsInline className={scss.video}>
-						<source src={"/images/mainload.mp4"} type="video/mp4" />
-						<a href="https://archive.org/details/BigBuckBunny_124">
-							download it
-						</a>
-					</video>
+					<video
+						src="/images/mainload.MP4"
+						autoPlay
+						muted
+						playsInline
+						style={{}}
+						className={scss.video}
+					></video>
 				</div>
 			) : (
 				<header className="app-header">
