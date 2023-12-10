e693d60a3d75bab399f8b8318d0d926c7286db2d
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-12-10 22:48:32 +0500

    updated

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import SEO from "@/components/SEO";
 export default function Document() {
 	return (
 		<Html lang="en">
 			<Head />
+			<SEO />
 			<link rel="icon" href="/images/head_logo.png" sizes="any" />
 			<body>
 				<Main />
